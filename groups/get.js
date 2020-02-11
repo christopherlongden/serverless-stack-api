2951e525f01b0bedcb8d8131e4b1e5c3218ffb62
Chris Longden <longden@longden.net>
2020-02-11 19:58:54 +0100

    Add get/update and change name parameter label in create

--- groups/get.js
+++ groups/get.js
@@ -1,25 +1,24 @@
-import uuid from "uuid";
 import * as dynamoDbLib from "../libs/dynamodb-lib";
 import { success, failure } from "../libs/response-lib";
 
 export async function main(event, context) {
-  const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.groupsTableName,
-    Item: {
+    Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
-      groupId: uuid.v1(),
-      groupName: data.groupName,
-      isPublic: data.isPublic,
-      createdAt: Date.now()
+      groupId: event.pathParameters.id
     }
   };
 
   try {
-    await dynamoDbLib.call("put", params);
-    return success(params.Item);
+    const result = await dynamoDbLib.call("get", params);
+    if (result.Item) {
+      // Return the retrieved item
+      return success(result.Item);
+    } else {
+      return failure({ status: false, error: "Item not found." });
+    }
   } catch (e) {
-    console.log(e);
     return failure({ status: false });
   }
 }
